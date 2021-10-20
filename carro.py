f7284bfc82157b310061b4a7f51abf0bac8c5e14
KauanTerrao_edutech-pr <86625808+KauanTerrao@users.noreply.github.com>
2021-10-20 16:40:22 -0300

    Add files via upload

---
+++ carro.py
@@ -0,0 +1,13 @@
+class Carro:
+    def __init__(self, quantindade_de_portas, quantidade_de_velocidades, marca, quantidade_de_cavalos, quantidade_de_cilindros, tipo_de_carro, placa, cor, cambio, direcao):
+        self.quantindade_de_portas = quantindade_de_portas
+        self.quantidade_de_velocidades = quantidade_de_velocidades
+        self.marca = marca
+        self.quantidade_de_cavalos = quantidade_de_cavalos
+        self.quantidade_de_cilindros = quantidade_de_cilindros
+        self.tipo_de_carro = tipo_de_carro
+        self.placa = placa
+        self.cor = cor
+        self.cambio = cambio
+        self.direcao = direcao
+
